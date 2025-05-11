4ead8577831a999663487705f4168aedda0f2a20
zamanov <zamanov@STHQ0122-01>
2025-05-11 18:19:42 +0400

    Add project files.

---
+++ NET 01. First project/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NET_01._First_project;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello .NET!!!");
+        Console.WriteLine("Hello C#");
+        Console.WriteLine("Hello rahatliq");
+    }
+}
---
+++ NET 01. Top Level Statement/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Hello .NET!!!");
+Console.WriteLine("Hello C#");
+Console.WriteLine("Hello rahatliq");
+Console.WriteLine("Hello Top Level Statement");

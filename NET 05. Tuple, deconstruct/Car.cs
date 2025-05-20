62044b7de9840844932f5a501ce9ffe0af832a82
Nadir Zamanov <Zamanov@itstep.edu.az>
2025-05-20 16:21:23 +0400

    Tuple, null

---
+++ NET 05. Tuple, deconstruct/Car.cs
@@ -0,0 +1,29 @@
+﻿// Deconstruct
+class Car
+{
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public int Year { get; set; }
+
+    public Car(string model, string manufacturer, int year)
+    {
+        Model = model;
+        Manufacturer = manufacturer;
+        Year = year;
+    }
+
+    // Deconstruct
+    // https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/functional/deconstruct
+
+    public void Deconstruct(out string model, out string manufacturer, out int year)
+    {
+        model = Model;
+        manufacturer = Manufacturer;
+        year = Year;
+    }
+
+    public override string ToString()
+    {
+        return $"Model: {Model}, Manufacturer: {Manufacturer}, Year: {Year}";
+    }
+}
---
+++ NET 05. Tuple, deconstruct/Program.cs
@@ -0,0 +1,45 @@
+﻿// tuple - new in C# 7
+//(int, double) myTuple = (5, 15.5);
+//Console.WriteLine(myTuple.Item1);
+//Console.WriteLine(myTuple.Item2);
+
+//(int summ, double price, string name) myTuple2 = (2569, 25.6, "Nadir");
+//Console.WriteLine(myTuple2.summ);
+//Console.WriteLine(myTuple2.price);
+//Console.WriteLine(myTuple2.name);
+
+//var result = CalculateSumAndAverage(5, 2);
+//int some;
+//(int summ, float aver, some) = CalculateSumAndAverage(5, 2);
+
+//Console.WriteLine(result.Item1);
+//Console.WriteLine(result.Item2);
+//Console.WriteLine(result.Item3);
+
+//Console.WriteLine(summ);
+//Console.WriteLine(aver);
+//Console.WriteLine(some);
+
+//(_, float aver1, _) = CalculateSumAndAverage(5, 2);
+//Console.WriteLine(aver1);
+
+Car car = new("Prius", "Toyota", 2025);
+Car car1 = new("e6", "BYD", 2036);
+//Console.WriteLine(car);
+//var model = car.Model;
+//var maufacturer = car.Manufacturer;
+//var year = car.Year;
+
+(string model, string manufacturer, int year) = car;
+(_, var manuf, _) = car;
+//Console.WriteLine(model);
+//Console.WriteLine(manufacturer);
+//Console.WriteLine(year);
+Console.WriteLine(manuf);
+
+(int, float, int) CalculateSumAndAverage(int numb1, int numb2)
+{
+    int sum = numb1 + numb2;
+    float average = sum / 2.0f;
+    return (sum, average, 786);
+}

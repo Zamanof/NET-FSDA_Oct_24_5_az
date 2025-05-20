dcd6ff1f7a0932af145b7a3f05b67ccb77263dfb
Nadir Zamanov <Zamanov@itstep.edu.az>
2025-05-20 17:42:34 +0400

    Operator overloading

---
+++ NET 05. Operator overloading/Point.cs
@@ -0,0 +1,118 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Point()
+        :this(default, default)
+    { }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    // operator overloading
+    // Unary operators overloading
+    
+    //Unary -
+    public static Point operator-(Point point) {
+        return new(-point.X, -point.Y);
+    }
+
+    public static Point operator++(Point point)
+    {
+        point.X++;
+        point.Y++;
+        return point;
+    }
+
+    public static Point operator --(Point point)
+    {
+        point.X--;
+        point.Y--;
+        return point;
+    }
+
+    // Binary operators overloading
+    public static Point operator+(Point left, Point right)
+    {
+        return new() { X = left.X + right.X, Y=left.Y+right.Y};
+    }
+    public static double operator -(Point left, Point right)
+    {
+        //Point tmp = new Point(left.X, right.X);
+        return Math.Sqrt(Math.Pow(left.Y - right.Y, 2.0)+ Math.Pow(left.X - right.X, 2.0));
+    }
+    public static Point operator *(Point point, int value)
+    {
+        return new() { X = point.X * value, Y = point.Y * value };
+    }
+    public static Point operator *(int value, Point point)
+    {
+        return new() { X = point.X * value, Y = point.Y * value };
+    }
+
+    // comparison operators
+    public override bool Equals(object? obj)
+    {
+        return this.ToString() == obj?.ToString();
+    }
+    public override int GetHashCode()
+    {
+        return this.ToString().GetHashCode();
+    }
+
+    public static bool operator ==(Point left, Point right)
+    {
+        return left.Equals(right);
+    }
+    public static bool operator !=(Point left, Point right)
+    {
+        return !(left ==right);
+    }
+
+    public static bool operator>(Point left, Point right)
+    {
+        return Math.Sqrt(Math.Pow(left.Y, 2.0) + Math.Pow(left.X, 2.0)) 
+            > Math.Sqrt(Math.Pow(right.Y, 2.0) + Math.Pow(right.X, 2.0));
+    }
+
+    public static bool operator <(Point left, Point right)
+    {
+        return Math.Sqrt(Math.Pow(left.Y, 2.0) + Math.Pow(left.X, 2.0))
+            < Math.Sqrt(Math.Pow(right.Y, 2.0) + Math.Pow(right.X, 2.0));
+    }
+
+    public static bool operator true(Point point) {
+        return point.X != 0 || point.Y != 0;
+    }
+    public static bool operator false(Point point) {
+        return point.X == 0 && point.Y == 0;
+    }
+
+    public static Point operator |(Point left, Point right)
+    {
+        if ((left.X != 0) || (left.Y != 0) || (right.X != 0) || (right.Y != 0))
+        {
+            return right;
+        }
+        return new();
+    }
+
+    public static Point operator &(Point left, Point right)
+    {
+        if ((left.X != 0) && (left.Y != 0) && (right.X != 0) && (right.Y != 0))
+        {
+            return right;
+        }
+        return new();
+    }
+
+    public override string ToString()
+    {
+        return $"Point ({X}, {Y})";
+    }
+}
---
+++ NET 05. Operator overloading/Program.cs
@@ -0,0 +1,39 @@
+﻿// Operator overloading
+Point point1 = new(0, 0);
+Point point2 = new(25, 23);
+//Console.WriteLine(point1);
+//Console.WriteLine(point2);
+
+//Point point3 = -point1;
+//Console.WriteLine(point3);
+//Console.WriteLine(-point2);
+
+//point1++;
+//Console.WriteLine(point1 + point2);
+//Console.WriteLine(point1 - point2);
+
+//Console.WriteLine(point1 * 3);
+//Console.WriteLine(3 * point1);
+
+
+//Console.WriteLine(point1 + point2);
+
+//Console.WriteLine(point1.Equals(point2));
+//Console.WriteLine(point1.GetHashCode());
+//Console.WriteLine(point2.GetHashCode());
+
+//Console.WriteLine(point2 == point1);
+//Console.WriteLine(point2 != point1);
+//Console.WriteLine(point2 > point1);
+//Console.WriteLine(point2 < point1);
+
+if (point1)
+{
+    Console.WriteLine("Koordinat bashlanqicinda deyil");
+}
+else
+{
+
+    Console.WriteLine("Koordinat bashlanqicindadir");
+}
+

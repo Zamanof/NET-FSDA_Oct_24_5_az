9b687615670402ff2f29312df9f3bfedd7d083ca
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-05-29 17:59:01 +0400

    delegates

---
+++ NET 09. Delegates/Human.cs
@@ -0,0 +1,12 @@
+﻿// delgates
+// delegate return_datatype delegate_name([parameter list])
+internal class Human
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} - > {Age}";
+    }
+}

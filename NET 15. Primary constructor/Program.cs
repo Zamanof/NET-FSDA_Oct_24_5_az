263634f530d363b38f5caa6f0bbe70b17d5182b3
Nadir Zamanov <Zamanov@itstep.edu.az>
2025-06-17 17:53:19 +0400

    Yield, Primary constructor

---
+++ NET 15. Primary constructor/Program.cs
@@ -0,0 +1,41 @@
+﻿// primary constructor -> new in C# 12
+// https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/tutorials/primary-constructors
+
+
+
+#region classic constructor
+//Dog dog = new("Toplan", 5, 5.5f);
+//dog.Print();
+//class Dog
+//{
+//    string name;
+//    int age;
+//    float price;
+
+//    // classic constructor
+//    public Dog(string name, int age, float price)
+//    {
+//        this.name = name;
+//        this.age = age;
+//        this.price = price;
+//    }
+//    public void Print()
+//    {
+//        Console.WriteLine($"{name} {age} {price}");
+//    }
+//}
+#endregion
+
+#region primary constructor
+Dog dog = new("Toplan", 5, 5.5f);
+dog.Print();
+
+class Dog(string name, int age, float price)
+{
+    public void Print()
+    {
+        Console.WriteLine($"{name} {age} {price}");
+    }
+}
+
+#endregion
---
+++ NET 15. Yield/Program.cs
@@ -0,0 +1,67 @@
+﻿// yield return, yield break
+
+var hellos = GetResults();
+//hellos.MoveNext();
+//Console.WriteLine(hellos.Current);
+//hellos.MoveNext();
+//Console.WriteLine(hellos.Current);
+//hellos.MoveNext();
+//Console.WriteLine(hellos.Current);
+//hellos.MoveNext();
+//Console.WriteLine(hellos.Current);
+//hellos.MoveNext();
+//Console.WriteLine(hellos.Current);
+//hellos.MoveNext();
+//Console.WriteLine(hellos.Current);
+
+//while (hellos.MoveNext())
+//{
+//    Console.WriteLine(hellos.Current);
+//}
+
+//foreach(var item in GetValues())
+//{
+//    Console.WriteLine(item);
+//}
+
+string lorem = "orem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur interdum ligula, a malesuada quam auctor vel. In porta justo congue malesuada fermentum.";
+
+foreach (var i in MyRange(5, lorem.Length, 3))
+{
+    Console.Write($"{lorem[i]} ");
+}
+
+
+
+IEnumerator<string> GetResults()
+{
+    yield return "Salam";
+    yield return "Hi";
+    yield return "Hello";
+    yield return "Ola";
+    yield return "Salam Aleykum";
+    yield return "Aloha";
+}
+IEnumerable<string> GetValues()
+{
+    List<string> strings = ["Saqol", "Bye", "Goodye", "Elvida", "Aloha", "Salam Aleykum"];
+
+    for (int i = 0; i < strings.Count; i++)
+    {
+        yield return strings[i].ToUpper();
+    }
+}
+
+IEnumerable<int> MyRange(int start, int? end = null, int steps = 1)
+{
+    if (start > end && steps > 0) yield break;
+    if (end is null)
+    {
+        end = start;
+        start = 0;
+    }
+    for (int i = start; i < end; i+= steps)
+    {
+        yield return i;
+    }
+}

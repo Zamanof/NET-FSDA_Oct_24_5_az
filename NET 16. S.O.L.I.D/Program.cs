f1c4cc62300776c1299a79d307fc85b368e7ab03
Nadir Zamanov <Zamanov@itstep.edu.az>
2025-06-19 18:07:44 +0400

    SOLID

---
+++ NET 16. S.O.L.I.D/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 
+                        S.O.L.I.D.
+
+1. Single Responcibility Principle (SRP) - vahid mesuliyyrt prinsipi
+    - There should never be more than one reason for a class to change.
+    - Class-in deyishdirilmesi uchun yalniz bir sebeb olmalidir
+
+2. Open-Closed Principle (OCP) - Achiq/qapali prinsipi
+    - Software entities ... should be open for extension, but closed for modification
+    - Proqramda olan "varliqlar" genishlenmeya achiq, deyishilmeye qapali olmalidir
+
+3. Liskov Substitution Principle(LSP) - Liskov evezetme prinsipi
+    - Functions that use pointers or references to base classes 
+        must be able to use objects of derived classes without knowing it
+    - Baza tipin istifade etdiyi funksionalliqlari, alttiplerimiz xeberdar olmasalar da
+        bele istifade ede bilmelidir
+*/
---
+++ NET 16. SOLID 1. Single Responcibility/Program.cs
@@ -0,0 +1,18 @@
+﻿// Single Responcibility
+
+#region Bad Example
+using System.Text.RegularExpressions;
+
+class SendMail
+{
+    public bool Send(string mailAddress, string message)
+    {
+        //if ()
+        return true;
+    }
+    private bool IsMailAddressValid(string mailAddress){
+        Regex mailPattern = new Regex(@"^[\w\.-]+@[\w\.-]+\.\w{2,}$");
+        return mailPattern.IsMatch(mailAddress);
+}
+}
+#endregion
